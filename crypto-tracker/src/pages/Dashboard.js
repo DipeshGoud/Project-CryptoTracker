6aee8315fea9bf2239b631cb9d2c5227b76bf6b9
DipeshGoud <dipeshgaud64@gmail.com>
2023-09-16 16:37:38 +0530

    update1

--- crypto-tracker/src/pages/Dashboard.js
+++ crypto-tracker/src/pages/Dashboard.js
@@ -1,29 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Header from '../components/Header'
-import TabsComponent from '../components/DashboardPage/Tabs'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Header from '../components/Header';
+import TabsComponent from '../components/DashboardPage/Tabs';
+import Search from '../components/DashboardPage/Search';
+import PaginationComponent from '../components/DashboardPage/Pagination';
 
 function DashboardPage() {
+    // State variables
+    let [coins, setCoins] = useState([]); // Holds all coins data
+    let [PaginationCoins, setPaginationCoins] = useState([]); // Holds coins data for the current page
+    const [search, setSearch] = useState(''); // Holds the search input value
+    const [page, setPage] = useState(1); // Holds the current page number
 
-    let [coins, setCoins] = useState([]);
+    // Function to handle page changes
+    const handlePageChange = (event, value) => {
+        setPage(value); // Update the current page number
+        var previousIndex = (value - 1) * 12; // Calculate the starting index for the current page
+        setPaginationCoins(coins.slice(previousIndex, previousIndex + 12)); // Set the coins data for the current page
+    };
 
-    let url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en';
+    // Function to handle search input changes
+    const onSearchChange = (e) => {
+        setSearch(e.target.value); // Update the search input value
+    }
 
+    // Filter coins based on search input
+    var filteredCoins = coins.filter(coin =>
+        coin.name.toLowerCase().includes(search.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(search.toLowerCase())
+    );
+
+    // API URL for fetching data
+    let url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=120&page=1&sparkline=false&locale=en';
+
+    // Fetch data from the API when the component mounts
     useEffect(() => {
         axios.get(url)
-        .then(res => {
-            console.log(res.data);
-            setCoins(res.data); 
-        })
-        .catch(error => console.error(error));
+            .then(res => {
+                setCoins(res.data); // Set the coins data
+                setPaginationCoins(res.data.slice(0, 12)); // Set the coins data for the initial page (first 12 coins)
+            })
+            .catch(error => console.error(error));
     }, []);
 
-return (
-    <div>
-        <Header />
-        <TabsComponent coins={coins}/>
-    </div>
-)
+    return (
+        <div>
+            <Header />
+            <Search search={search} onSearchChange={onSearchChange} />
+            <TabsComponent coins={search ? filteredCoins : PaginationCoins} />
+            {!search && <PaginationComponent page={page} handlePageChange={handlePageChange} />}
+        </div>
+    );
 }
 
-export default DashboardPage
+export default DashboardPage;
